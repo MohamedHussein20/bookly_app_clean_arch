bb7d8fb238de68ef877c26a7bc6beb49f9070f3d
Mohamed Hussein <mohussein1279@gmail.com>
2025-10-02 01:14:21 +0300

    make book model extends book entity

--- lib/features/home/data/models/book_model/book_model.dart
+++ lib/features/home/data/models/book_model/book_model.dart
@@ -1,28 +1,37 @@
+import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+
 import 'access_info.dart';
 import 'sale_info.dart';
 import 'search_info.dart';
 import 'volume_info.dart';
 
-class BookModel {
+class BookModel extends BookEntity {
   final String? kind;
   final String? id;
   final String? etag;
   final String? selfLink;
-  final VolumeInfo volumeInfo;
+  final VolumeInfo? volumeInfo;
   final SaleInfo? saleInfo;
   final AccessInfo? accessInfo;
   final SearchInfo? searchInfo;
 
-  const BookModel({
+   BookModel({
     this.kind,
     this.id,
     this.etag,
     this.selfLink,
-    required this.volumeInfo,
+    this.volumeInfo,
     this.saleInfo,
     this.accessInfo,
     this.searchInfo,
-  });
+  }) : super(
+         bookId: id!,
+         image: volumeInfo?.imageLinks?.thumbnail ?? '',
+         title: volumeInfo!.title!,
+         authorName: volumeInfo.authors!.first,
+         price: 0.0,
+         rating: volumeInfo.averageRating,
+       );
 
   factory BookModel.fromJson(Map<String, dynamic> json) => BookModel(
     kind: json['kind'] as String?,
@@ -46,7 +55,7 @@ class BookModel {
     'id': id,
     'etag': etag,
     'selfLink': selfLink,
-    'volumeInfo': volumeInfo.toJson(),
+    'volumeInfo': volumeInfo?.toJson(),
     'saleInfo': saleInfo?.toJson(),
     'accessInfo': accessInfo?.toJson(),
     'searchInfo': searchInfo?.toJson(),
