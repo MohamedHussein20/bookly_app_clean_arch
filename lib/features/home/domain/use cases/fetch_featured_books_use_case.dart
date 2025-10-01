15bdb271ceb18b3c93d520c806809377674b433f
Mohamed Hussein <mohussein1279@gmail.com>
2025-10-01 20:38:08 +0300

    create Fetch Featured Books Use Case

---
+++ lib/features/home/domain/use cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/domain/entities/book_entitiy.dart';
+import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+
+  FetchFeaturedBooksUseCase(this.homeRepo);
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
