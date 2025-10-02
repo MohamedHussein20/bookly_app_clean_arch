8bbcf469e8042969b958f4bcebc6d29959375b8b
Mohamed Hussein <mohussein1279@gmail.com>
2025-10-02 21:57:30 +0300

    create featured books state

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,10 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
