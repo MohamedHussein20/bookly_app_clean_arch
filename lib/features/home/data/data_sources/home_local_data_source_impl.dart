67d77f51ff9dbb1de3c85e67249b919d0a8bce79
Mohamed Hussein <mohussein1279@gmail.com>
2025-10-02 16:08:59 +0300

    fetch featured books local data source

--- lib/features/home/data/data_sources/home_local_data_source_impl.dart
+++ lib/features/home/data/data_sources/home_local_data_source_impl.dart
@@ -1,11 +1,13 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/home/data/data_sources/home_local_data_source.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:hive/hive.dart';
 
 class HomeLocalDataSourceImpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+    var box = Hive.box<BookEntity>(kFeaturedBox);
+    return box.values.toList();
   }
 
   @override
