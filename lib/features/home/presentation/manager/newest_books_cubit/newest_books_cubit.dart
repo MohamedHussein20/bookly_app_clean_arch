b443f42857e4b42cc7b913aa3a4600c88e9c4f6a
Mohamed Hussein <mohussein1279@gmail.com>
2025-10-02 22:11:24 +0300

    create newest books cubit

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/domain/use%20cases/fetch_newest_books_use_case.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.newestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase newestBooksUseCase;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await newestBooksUseCase.call();
+    result.fold((failure) {
+      emit(NewestBooksFailure(failure.errMessage));
+    }, (books) {
+      emit(NewestBooksSuccess(books));
+    });
+  }
+}
