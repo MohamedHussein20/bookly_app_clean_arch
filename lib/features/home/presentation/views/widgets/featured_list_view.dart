89681c9b784319c2a7e4402450d4e0f1bad289a6
Mohamed Hussein <mohussein1279@gmail.com>
2025-10-03 13:02:01 +0300

    pagination part two

--- lib/features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,26 +1,53 @@
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/featured_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class FeaturedBookListView extends StatelessWidget {
+class FeaturedBookListView extends StatefulWidget {
   const FeaturedBookListView({super.key, required this.books});
   final List<BookEntity> books;
+
+  @override
+  State<FeaturedBookListView> createState() => _FeaturedBookListViewState();
+}
+
+class _FeaturedBookListViewState extends State<FeaturedBookListView> {
+  final ScrollController _scrollController = ScrollController();
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels >=
+        0.7 * _scrollController.position.maxScrollExtent) {
+      BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+    }
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.25,
       child: ListView.builder(
+        controller: _scrollController,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
           return Padding(
             padding: EdgeInsets.only(left: 4.w, right: 16.w),
-            child:  FeaturedItem(
-             image:  books[index].image ?? ''
-            ),
+            child: FeaturedItem(image: widget.books[index].image ?? ''),
           );
         },
-        itemCount: books.length,
+        itemCount: widget.books.length,
       ),
     );
   }
